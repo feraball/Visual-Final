9635afba88ac1045bc22a93066abadab2234e2fb
Fernando Balladares <balladares.fernando.92@gmail.com>
2015-08-28 13:16:44 -0500

    wfwf
    wfwf

--- Proyecto Final/MainWindow.xaml.vb
+++ Proyecto Final/MainWindow.xaml.vb
@@ -1,11 +1,20 @@
 ﻿Class MainWindow 
     Private ventanaRegistro As RegistroUsuario
+    Private ventanaCliente As VentanaCliente
 
     Private Sub btnSignin_Click(sender As Object, e As RoutedEventArgs) Handles btnSignin.Click
         ventanaRegistro = New RegistroUsuario
         ventanaRegistro.Owner = Me
         ventanaRegistro.Show()
         Me.IsEnabled = False
+
+    End Sub
+
+    Private Sub btnLogin_Click(sender As Object, e As RoutedEventArgs) Handles btnLogin.Click
+        ventanaCliente = New VentanaCliente
+        ventanaCliente.Owner = Me
+        ventanaCliente.Show()
+        Me.Hide()
 
     End Sub
 End Class
