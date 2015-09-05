9e55e49c38cdbbf6ba4c52c1d8bb206a1f816dee
Fernando Balladares <balladares.fernando.92@gmail.com>
2015-09-05 14:14:35 -0500

    Union
    d

--- Proyecto Final/MainWindow.xaml.vb
+++ Proyecto Final/MainWindow.xaml.vb
@@ -1,14 +1,9 @@
 ﻿Class MainWindow 
-    Private ventanaRegistro As RegistroUsuario
+
     Private ventanaCliente As VentanaCliente
 
-    Private Sub btnSignin_Click(sender As Object, e As RoutedEventArgs) Handles btnSignin.Click
-        ventanaRegistro = New RegistroUsuario
-        ventanaRegistro.Owner = Me
-        ventanaRegistro.Show()
-        Me.IsEnabled = False
-
-    End Sub
+    Dim imagenCorrecto As BitmapImage = New BitmapImage(New Uri("Assets/check.png", UriKind.Relative))
+    Dim imagenError As BitmapImage = New BitmapImage(New Uri("Assets/error.png", UriKind.Relative))
 
     Private Sub btnLogin_Click(sender As Object, e As RoutedEventArgs) Handles btnLogin.Click
         ventanaCliente = New VentanaCliente
@@ -53,6 +48,30 @@
     Private Sub txtPass_PreviewKeyDown(sender As Object, e As KeyEventArgs) Handles txtPass.PreviewKeyDown
         If e.Key = Key.Space Then
             e.Handled = True
+        End If
+    End Sub
+
+
+
+    Private Sub txtNewPass1_LostFocus(sender As Object, e As RoutedEventArgs) Handles txtNewPass1.LostFocus
+        If txtNewPass1.Password = String.Empty Then
+            imgPass1.Visibility = Windows.Visibility.Hidden
+        End If
+    End Sub
+
+    Private Sub txtNewPass2_PreviewKeyUp(sender As Object, e As KeyEventArgs) Handles txtNewPass2.PreviewKeyUp
+        imgPass2.Visibility = Windows.Visibility.Visible
+        If txtNewPass1.Password = txtNewPass2.Password Then
+            imgPass2.Source = imagenCorrecto
+        Else
+            imgPass2.Source = imagenError
+        End If
+    End Sub
+
+    Private Sub txtNewPass1_PreviewKeyUp(sender As Object, e As KeyEventArgs) Handles txtNewPass1.PreviewKeyUp
+        If Not (txtNewPass1.Password = String.Empty) Then
+            imgPass1.Visibility = Windows.Visibility.Visible
+            imgPass1.Source = imagenCorrecto
         End If
     End Sub
 End Class
